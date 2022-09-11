809c4bc2c7ca8af6233198173647f1ad2eaa4b97
gobit0dic <elkloso@gmail.com>
2022-09-11 18:30:25 +0200

    Test Config

---
+++ src/test/java/com/example/lockpicking/ConfigTests.java
@@ -0,0 +1,13 @@
+package com.example.lockpicking;
+
+public class ConfigTests {
+
+    public void tests(){
+        Config config = new Config();
+        config.picks = new String[]{};
+        config.wrenches = new String[]{};
+        config.locks = new String[]{};
+        config.pins = new String[]{};
+    }
+
+}
