fffdea7f8ec22d0aee556621785c000f5582088e
gobit0dic <elkloso@gmail.com>
2022-09-11 18:33:30 +0200

    Test ConfigHandler

--- src/test/java/com/example/lockpicking/ConfigHandlerTests.java
+++ src/test/java/com/example/lockpicking/ConfigHandlerTests.java
@@ -1,4 +1,18 @@
 package com.example.lockpicking;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ConfigHandlerTests {
+
+    @Test
+    public void getInstance(){
+        ConfigHandler configHandler = ConfigHandler.getInstance();
+        Config config = configHandler.config;
+        assert(config.picks.length != 0);
+        assert(config.wrenches.length != 0);
+        assert(config.locks.length != 0);
+        assert(config.pins.length != 0);
+    }
 }
