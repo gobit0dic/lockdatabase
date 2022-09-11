2cca28709dfee7f705d9a7d5602487162b2c1f7d
gobit0dic <elkloso@gmail.com>
2022-09-11 18:30:55 +0200

    Test Config

--- src/test/java/com/example/lockpicking/ConfigTests.java
+++ src/test/java/com/example/lockpicking/ConfigTests.java
@@ -1,7 +1,12 @@
 package com.example.lockpicking;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ConfigTests {
 
+    @Test
     public void tests(){
         Config config = new Config();
         config.picks = new String[]{};
