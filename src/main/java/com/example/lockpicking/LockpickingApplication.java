12fd36cda5acbf73fe5ddc63d48dcea2a6cbd94e
gobit0dic <elkloso@gmail.com>
2022-09-12 15:45:01 +0200

    Bugfix Dependency Error

--- src/main/java/com/example/lockpicking/LockpickingApplication.java
+++ src/main/java/com/example/lockpicking/LockpickingApplication.java
@@ -3,12 +3,8 @@ package com.example.lockpicking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class LockpickingApplication {
 
     public static void main(String[] args) {
